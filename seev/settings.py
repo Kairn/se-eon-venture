a518f694fb1cb09748f1bd4c9c101cd1239e9f59
Eddy Soma <esoma92.dev@gmail.com>
2019-07-02 23:44:52 -0500

    add global static files path and basic scss

--- seev/settings.py
+++ seev/settings.py
@@ -20,6 +20,8 @@ DB_ENV = os.environ['SEEV_AGGRE'].split(';@')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Source path
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -128,3 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Global static resources
+STATICFILES_DIRS = [
+    os.path.join(SRC_DIR, 'resources', 'images'),
+    os.path.join(SRC_DIR, 'resources', 'css'),
+    os.path.join(SRC_DIR, 'resources', 'js'),
+]
