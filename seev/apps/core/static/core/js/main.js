b4db7fefeff2a6086907d7ae2a4ffb37f56bfbc7
Eddy Soma <esoma92.dev@gmail.com>
2019-07-05 19:47:25 -0500

    link core app template with base and map static files

---
+++ seev/apps/core/static/core/js/main.js
@@ -0,0 +1,1 @@
+console.log('Core app is launching...');
--- seev/resources/js/globalUtils.js
+++ seev/resources/js/globalUtils.js
@@ -1,1 +1,4 @@
 // All constants and shared functions used by the application
+window.onload = () => {
+  console.log('Running application in DEBUG mode...');
+}
