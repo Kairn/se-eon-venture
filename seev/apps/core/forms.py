c37aaaf27363d64763ba59584228ff0bde18cb5d
Eddy Soma <esoma92.dev@gmail.com>
2019-07-20 14:41:02 -0500

    add login and reset dj-forms

--- seev/apps/core/forms.py
+++ seev/apps/core/forms.py
@@ -1,1 +1,62 @@
 from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        required=True,
+        label='Username',
+        error_messages={
+            'required': 'Please enter your username',
+        },
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-ti-field',
+                'placeholder': 'Username',
+            }
+        )
+    )
+    password = forms.CharField(
+        required=True,
+        label='Password',
+        error_messages={
+            'required': 'Please enter your password',
+        },
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-ti-field',
+                'placeholder': 'Password',
+            }
+        )
+    )
+
+
+class PasswordResetForm(forms.Form):
+    email = forms.EmailField(
+        required=True,
+        label='Email',
+        error_messages={
+            'required': 'Email address is required',
+        },
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-ti-field',
+                'placeholder': 'Email',
+            }
+        )
+    )
+    pin = forms.IntegerField(
+        required=True,
+        label='PIN',
+        max_value=9999,
+        min_value=1000,
+        error_messages={
+            'required': 'PIN number is required',
+            'invalid': 'Enter a valid PIN number',
+        },
+        widget=forms.NumberInput(
+            attrs={
+                'class': 'form-ti-field',
+                'placeholder': 'PIN',
+            }
+        )
+    )
