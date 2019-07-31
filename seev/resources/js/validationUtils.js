845054a91d600e65b14eabc0de40a479f32c9859
Eddy Soma <esoma92.dev@gmail.com>
2019-07-30 19:17:33 -0500

    add a few validation methods

--- seev/resources/js/validationUtils.js
+++ seev/resources/js/validationUtils.js
@@ -22,3 +22,65 @@ const disEnaButton = function(button, valid) {
     button.classList.add('form-btn-dis');
   }
 }
+
+// Validate if an input meets length requirement
+const isSuffLength = function(input, length) {
+  return input.length >= length;
+}
+
+// Validate an email address
+const isValidEmail = function(input) {
+  //
+};
+
+// Validate a U.S. phone number
+const isValidPhone = function(input) {
+  //
+};
+
+// Validate a PIN number
+const isValidPin = function(input) {
+  //
+};
+
+// Validate a user password
+const isPsStrong = function(input) {
+  // Upper case letter
+  let upRegex = new RegExp(`[A-Z]`);
+
+  // Lower case letter
+  let loRegex = new RegExp(`[a-z]`);
+
+  // Digit
+  let digRegex = new RegExp(`\\d`);
+
+  // Special character
+  let speRegex = new RegExp(`\\W`);
+
+  // No space character allowed
+  let spaRegex = new RegExp(`\\s`);
+
+  input = input.trim();
+
+  if (!isSuffLength(input, 8)) {
+    return false;
+  }
+
+  if (!upRegex.test(input)) {
+    return false;
+  }
+  if (!loRegex.test(input)) {
+    return false;
+  }
+  if (!digRegex.test(input)) {
+    return false;
+  }
+  if (!speRegex.test(input)) {
+    return false;
+  }
+  if (spaRegex.test(input)) {
+    return false;
+  }
+
+  return true;
+};
