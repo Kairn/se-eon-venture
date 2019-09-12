d3189a418a93661832dbca2e48521f3dc0e0cbf7
Eddy Soma <esoma92.dev@gmail.com>
2019-09-11 19:23:49 -0500

    add basic auth logic to catalog home with link from client page

--- seev/apps/catalog/views.py
+++ seev/apps/catalog/views.py
@@ -7,9 +7,19 @@ import traceback
 from django.db import transaction
 from django.shortcuts import render, redirect, reverse
 
+from seev.apps.core.models import UnoClient
+
 
 def go_cat_home(request, context=None):
-    if not context:
-        context = {}
+    try:
+        if not context:
+            context = {}
 
-    return render(request, 'catalog/index.html', context=context)
+        client = UnoClient.objects.get(client_id=request.session['id'])
+        context['client'] = client
+
+        return render(request, 'catalog/index.html', context=context)
+    except Exception:
+        if request and hasattr(request, 'session'):
+            request.session.clear()
+        return redirect('go_login')
