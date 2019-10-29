d22e714d2eed8f45eacf63591ee1a603cd701675
Eddy Soma <esoma92.dev@gmail.com>
2019-10-28 19:15:08 -0500

    implement oppo fetching with number id

--- seev/apps/order/urls.py
+++ seev/apps/order/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.go_ord_home, name='go_ord_home'),
-    # re_path(r'^?/$', views.?, name='?'),
+    re_path(r'^find-oppo/$', views.find_oppo_by_num, name='find_oppo_by_num'),
 ]
--- seev/apps/order/views.py
+++ seev/apps/order/views.py
@@ -25,3 +25,33 @@ def go_ord_home(request, context=None):
         context = {}
 
     return render(request, 'order/index.html', context=context)
+
+
+def find_oppo_by_num(request, context=None):
+    if request.method == 'POST':
+        try:
+            oppoNumber = request.POST['opportunity-number']
+
+            opportunity = UnoOpportunity.objects.get(
+                opportunity_number=oppoNumber)
+            if not opportunity.active or opportunity.deal_count >= opportunity.deal_limit:
+                raise AssertionError
+
+            # Fill opportunity details
+            oppoData = {}
+
+            context = {}
+            context['oppoData'] = oppoData
+
+            return render(request, 'order/index.html', context=context)
+        except AssertionError:
+            store_context_in_session(request, addSnackDataToContext(
+                context, 'Opportunity has expired'))
+            return redirect('go_ord_home')
+        except Exception:
+            traceback.print_exc()
+            store_context_in_session(request, addSnackDataToContext(
+                context, 'Opportunity not found'))
+            return redirect('go_ord_home')
+    else:
+        return redirect('go_ord_home')
