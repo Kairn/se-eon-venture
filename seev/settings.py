fecbc5f2ff9f7d9360337775876c99476aa994fc
Eddy Soma <esoma92.dev@gmail.com>
2020-01-11 16:11:04 -0600

    fix bugs in settings

--- seev/settings.py
+++ seev/settings.py
@@ -34,12 +34,7 @@ SECRET_KEY = os.environ['DJANGO_SEC_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '*'
-    'localhost',
-    '127.0.0.1',
-    '[::1]',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
