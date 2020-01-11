1a4689291cf49eab96836a8fd85fc89c081808ba
Eddy Soma <esoma92.dev@gmail.com>
2020-01-11 15:15:24 -0600

    GCE deployment file changes

--- seev/settings.py
+++ seev/settings.py
@@ -145,8 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_URL = '/static/'
-STATIC_URL = 'http://localhost:5555/'
+STATIC_URL = '/static/'
+# STATIC_URL = 'http://localhost:5555/'
 
 # Global static resources
 STATICFILES_DIRS = [
